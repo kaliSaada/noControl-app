a25085518b06e3d13f44e17e69f67fd4686764dd
kaliSaada <khalil062010@gmail.com>
2021-02-25 22:14:44 -0300

    fix: renamed files for tsc

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>TEste</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;

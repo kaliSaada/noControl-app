277a603737899339187e9e06bd429d5f36f58c24
kaliSaada <khalil062010@gmail.com>
2021-02-28 17:08:53 -0300

    feat: adding new package UI

---
+++ src/package/UI/components/input/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const InputBox = styled.View`
+  width: 90%;
+  max-width: 500px;
+  background-color: aliceblue;
+`;
+
+export const InputLabel = styled.Text`
+  font-size: 20px;
+`;
+
+export const InputError = styled.Text`
+  color: #f00;
+  font-size: 10px;
+`;
